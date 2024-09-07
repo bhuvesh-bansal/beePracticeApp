c83688776efed18604d0a00a8a1e4b8efd660978
Bhuvesh <bhuvesh1449.be22@chitkara.edu.in>
2024-09-07 19:54:30 +0530

    multer setup 1

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import{v2 as cloudinary} from "cloudinary"
+import exp from "constants";
+import fs from "fs"
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localfilepath) => {
+  try {
+    if (!localfilepath) return null
+    //upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localfilepath,{
+      resource_type : "auto"
+    })
+    //file has been uploaded successfully
+    console.log("File uploaded on cloudinary",response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localfilepath)//removes the locally saved temporary file as the upload operation failed
+    return null;
+  }
+}
+
+export {uploadOnCloudinary}
